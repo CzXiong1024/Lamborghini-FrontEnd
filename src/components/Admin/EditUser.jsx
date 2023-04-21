62ad3285b86fea7458c84e8efd9e1a96bcb7e0ff
CzXiong1024 <chx53@pitt.edu>
2023-04-20 22:58:23 -0400

    Initial commit

---
+++ src/components/Admin/EditUser.jsx
@@ -0,0 +1,115 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Helmet } from "react-helmet";
+import axios from 'axios';
+import { TextInput, Title, Button, Switch } from '@mantine/core';
+import { UserContext } from '../../context/userContext/UserContext';
+import { updateUser } from '../../context/userContext/apiCalls';
+
+const EditProduct = ({ editId, setEdit }) => {
+	const admin = JSON.parse(localStorage.getItem('user')).isAdmin;
+  const { dispatch } = useContext(UserContext);
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [profilePic, setProfilePic] = useState('');
+  const [isAdmin, setisAdmin] = useState(false);
+  
+  useEffect(() => {
+    const getUserData = async () => {
+      const response = await axios.get(`https://infsci2560-final-lambo-server.glitch.me/api/users/find/${editId}`);
+      const data = response.data.info;
+      setEmail(data.email);
+      setFirstName(data.firstName);
+      setLastName(data.lastName);
+      setProfilePic(data.profilePic);
+      setisAdmin(data.isAdmin);
+    }
+    getUserData();
+  }, [editId]);
+
+  const handleUpdate = () => {
+    const updatedData = {
+      id: editId,
+      email: email,
+      username: username,
+      firstName: firstName,
+      lastName: lastName,
+      profilePic: profilePic,
+      isAdmin: isAdmin,
+    }
+    updateUser(updatedData, dispatch);
+    setEdit(false);
+  }
+
+  return (
+    <>
+    <Helmet>
+      <title>Edit {username} | Apple E-Commerce</title>
+      <meta name='description' content='Edit Account' />
+    </Helmet>
+    {
+      admin ? 
+      <>
+      <Title order={2}>Edit {username}</Title>
+      <TextInput
+      label="Email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      id="email"
+      size="md"
+      required
+      />
+      <TextInput
+      label="Username"
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+      id="username"
+      size="md"
+      required
+      />
+      <TextInput
+      label="First Name"
+      value={firstName}
+      onChange={(e) => setFirstName(e.target.value)}
+      id="firstName"
+      size="md"
+      required
+      />
+      <TextInput
+      label="Last Name"
+      value={lastName}
+      onChange={(e) => setLastName(e.target.value)}
+      id="lastName"
+      size="md"
+      required
+      />
+      <TextInput
+      label="Account Avatar"
+      value={profilePic}
+      onChange={(e) => setProfilePic(e.target.value)}
+      id="profilePic"
+      size="md"
+      required
+      />
+      <Switch 
+      label="Admin Control" 
+      size="md" 
+      id="isAdmin" 
+      checked={isAdmin}
+      onChange={() => setisAdmin(!isAdmin)} 
+      style={{ marginTop: '10px' }}
+      />
+
+      <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
+        <Button type="Submit" variant="light" size="sm" onClick={handleUpdate}>Update User</Button>
+      </div>
+    </>
+      :
+    <div style={{ width: '100%', marginLeft: 'auto', marginRight: 'auto' }}><Title order={3}>You do not have permission to access this</Title></div>
+    }
+    </>
+  )
+}
+
+export default EditProduct
---
+++ src/context/productContext/apiCalls.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import { 
+  updateProductStart,
+  updateProductSuccess,
+  updateProductFailure,
+  createProductStart,
+  createProductSuccess,
+  createProductFailure,
+  deleteProductFailure, 
+  deleteProductStart, 
+  deleteProductSuccess, 
+  getProductsFailure, 
+  getProductsStart, 
+  getProductsSuccess 
+} from './ProductActions';
+
+// GET PRODUCTS
+export const getProducts = async (dispatch) => {
+  dispatch(getProductsStart())
+  try {
+    const res = await axios.get('https://infsci2560-final-lambo-server.glitch.me/api/products')
+    dispatch(getProductsSuccess(res.data))
+  } catch (error) {
+    dispatch(getProductsFailure())
+  }
+}
+
+// CREATE PRODUCT
+export const createProduct = async (product, dispatch) => {
+  dispatch(createProductStart())
+  try {
+    const res = await axios.post(`https://infsci2560-final-lambo-server.glitch.me/api/products/create`, product, {
+      headers: {
+        token: 'Bearer ' + JSON.parse(localStorage.getItem('user')).accessToken,
+      }
+    })
+    dispatch(createProductSuccess(res.data))
+  } catch (error) {
+    dispatch(createProductFailure())
+  }
+}
+
+// UPDATE PRODUCT
+export const updateProduct = async (product, dispatch) => {
+  dispatch(updateProductStart())
+  try {
+    const res = await axios.put(`https://infsci2560-final-lambo-server.glitch.me/api/products/update/${product.id}`, product, {
+      headers: {
+        token: 'Bearer ' + JSON.parse(localStorage.getItem('user')).accessToken,
+      }
+    })
+    console.log(res.data)
+    dispatch(updateProductSuccess(res.data))
+  } catch (error) {
+    dispatch(updateProductFailure())
+  }
+}
+
+// DELETE PRODUCT
+export const deleteProduct = async (id, dispatch) => {
+  dispatch(deleteProductStart())
+  try {
+    await axios.delete(`https://infsci2560-final-lambo-server.glitch.me/api/products/delete/${id}`, {
+      headers: {
+        token: 'Bearer ' + JSON.parse(localStorage.getItem('user')).accessToken,
+      }
+    })
+    dispatch(deleteProductSuccess(id))
+  } catch (error) {
+    dispatch(deleteProductFailure())
+  }
+}
